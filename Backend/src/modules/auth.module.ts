b3d11334e9d4ed244745760fc79cdf66532f4eee
Nizar <nairp6502@gmail.com>
2026-02-02 11:42:36 +0200

    changed name fro jwt strategy

--- Backend/src/modules/auth.module.ts
+++ Backend/src/modules/auth.module.ts
@@ -1,10 +1,9 @@
 // auth.module.ts
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from '../Services/Auth.service'
-import { JwtStrategy } from '../Services/Jwt.strategy'
+import { JwtStrategy } from '../Services/jwt.strategy'
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { ITokenService } from 'src/Services/IToken.service';
 import { TokenModule } from './token.module';
 import { PlayerModule } from './player.module';
 import { FaceitStrategy } from 'src/Services/Faceit.strategy';
